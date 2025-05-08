5189edff8c309511980e829980a131a77799080a
kuromisdad <jessicabahen03@gmail.com>
2025-05-08 21:02:21 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ function login() {
     const password = document.getElementById("password").value;
     const error = document.getElementById("error-msg");
   
-    // Customize your login credentials here
+    // Your login credentials
     const validUsername = "caleb";
     const validPassword = "secretspace2003";
   
-    if (username === validUsername && password === validPassword) {
-      window.location.href = "home.html"; // Redirect to personal space
+    if (
+      username.toLowerCase() === validUsername.toLowerCase() &&
+      password === validPassword
+    ) {
+      window.location.href = "home.html"; // Go to your personal page
     } else {
       error.textContent = "Incorrect login. Try again.";
     }
